f47f449c2bd95ddc1f5f7b708d0bc35bf110776d
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-12-18 23:35:52 +0530

    Modified: Search person in a city or a state

--- Contacts.java
+++ Contacts.java
@@ -1,90 +1,70 @@
-package com.bridgelabz;
+package com.bridgelab;
 
-	/*
-	 * Ability to create a Contacts in Address Book with first and last names,
-	 * address, city, state, zip, phone number and email...
-	 */
-	public class Contacts {
+public class ContactPerson {
 
-	    private String firstName, lastName, address, city, state, email;
-	    private int zipCode;
-	    private long phoneNumber;
+    private String firstName,lastName,address,city,state,email;
+    private int zipCode;
+    private long phoneNumber;
 
-	    public String getFirstName() {
-	        return firstName;
-	    }
+    public ContactPerson()  {
 
-	    public String getLastName() {
-	        return lastName;
-	    }
+    }
 
-	    public String getAddress() {
-	        return address;
-	    }
+    public String getFirstName() {
+        return firstName;
+    }
 
-	    public String getCity() {
-	        return city;
-	    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	    public String getState() {
-	        return state;
-	    }
-	    
-	    public String getEmail() {
-	        return email;
-	    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	    public int getZipCode() {
-	        return zipCode;
-	    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	    public long getPhoneNumber() {
-	        return phoneNumber;
-	    }
+    public String getCity(){
+        return city;
+    }
 
-	    public void setFirstName(String firstName) {
-	        this.firstName = firstName;
-	    }
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	    public void setLastName(String lastName) {
-	        this.lastName = lastName;
-	    }
+    public String getState(){
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
 
-	    public void setAddress(String address) {
-	        this.address = address;
-	    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	    public void setCity(String city) {
-	        this.city = city;
-	    }
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
 
-	    public void setState(String state) {
-	        this.state = state;
-	    }
-	    
-	    public void setEmail(String email) {
-	        this.email = email;
-	    }
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-	    public void setZipCode(int zipCode) {
-	        this.zipCode = zipCode;
-	    }
 
-	    public void setPhoneNumber(long phoneNumber) {
-	        this.phoneNumber = phoneNumber;
-	    }
-	    
-	    @Override
-	    public String toString() {
-	        return "Contacts{" +
-	                "firstName='" + firstName + '\'' +
-	                ", lastName='" + lastName + '\'' +
-	                ", address='" + address + '\'' +
-	                ", city='" + city + '\'' +
-	                ", state='" + state + '\'' +
-	                ", email='" + email + '\'' +
-	                ", zipCode=" + zipCode +
-	                ", phoneNumber=" + phoneNumber +
-	                '}';
-	    }
-	}
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", email='" + email + '\'' +
+                ", zip=" + zipCode +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
